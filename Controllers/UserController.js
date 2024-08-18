2fdef80d2f4f3e11181487ec35473234b84c879c
gabrielcaetano1 <gabrielcaetano2319@gmail.com>
2024-08-18 16:04:27 -0300

    Ta dando tudo certo

---
+++ Controllers/UserController.js
@@ -0,0 +1,57 @@
+import { User } from "../Models/User.js";
+import { doCompare, doHash } from "../Middlewares/Hash.js";
+
+// Get - Listar todos os usuários
+export const getAllUsers = async (request, response) => {
+  try {
+    const users = await User.findAll();
+    response.status(200).json(users);
+  } catch (error) {
+    response.status(500).json({ error: error.message });
+  }
+};
+
+// Get - Listar usuário por ID
+// export const getUserById = async (request, response, next) => {
+//   const userId = parseInt(request.params.id);
+//   const user = await User.findByPk(userId,);
+
+//   if(!user) {
+//       const error = new Error("Usuário não encontrado!");
+//       error.statusCode = 404;
+//       response.json({ user:  null });
+//   }
+// };
+
+// Post - Criar Usuário
+export const registerUser = async (req, res) => {
+  try {
+
+    const { nome, email, senha, cpf } = req.body;
+
+    const hashedPassword = doHash(senha);
+    const users = await User.create({nome, email, senha: hashedPassword, cpf});
+
+    res.status(201).json(users);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+// POST - LOGIN
+
+export const loginUser = async (request, response) => {
+  try {
+    const { email, senha} = request.body;
+    const user = await User.findOne({ where: { email } });
+
+    if(doCompare(senha, user.senha)) {
+      response.status(200).json({message: 'Login efetuado com sucesso!'});
+    } else {
+      response.status(401).json({ error: 'Credenciais inválidas!' });
+    }
+
+  } catch (error) {
+    console.log(error);
+  };
+};
---
+++ Models/User.js
@@ -0,0 +1,29 @@
+import { Sequelize } from "sequelize";
+import { DBconfig } from "../Config/db.js";
+
+export const User = DBconfig.define("usuario", {
+  nome: {
+    type: Sequelize.STRING(50),
+    allowNull: true, 
+  },
+  email: {
+    type: Sequelize.STRING(100), 
+    allowNull: false, 
+    unique: true, 
+    validate: {
+      isEmail: true, 
+    }
+  },
+  senha: {
+    type: Sequelize.STRING(500), 
+    allowNull: false, 
+  },
+  cpf: {
+    type: Sequelize.STRING(14), 
+    allowNull: false, 
+    unique: true,
+    validate: {
+      is: /^[0-9]{3}\.[0-9]{3}\.[0-9]{3}-[0-9]{2}$/i, 
+    }
+  }
+});
